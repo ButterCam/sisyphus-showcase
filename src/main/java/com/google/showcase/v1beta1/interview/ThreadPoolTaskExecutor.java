2fd136b6839e3efd38574b999c97a684b629ec4e
higan <higan@live.cn>
2022-09-22 14:52:46 +0800

    :sparkles: Add thread pool task

---
+++ src/main/java/com/google/showcase/v1beta1/interview/ThreadPoolTaskExecutor.java
@@ -0,0 +1,27 @@
+package com.google.showcase.v1beta1.interview;
+
+import java.util.Random;
+
+// 03. 请实现一个多线程任务执行器
+public class ThreadPoolTaskExecutor {
+    public static Random random = new Random();
+
+    public static int[] tasks = new int[100000];
+
+    static {
+        for (int i = 0; i < tasks.length; i++) {
+            tasks[i] = i;
+        }
+    }
+
+    // 这是我们需要执行的方法。
+    public static void execute(int taskId) throws InterruptedException {
+        Thread.sleep(random.nextInt(500));
+        System.out.println(taskId);
+    }
+
+    // 请在这里实现多线程的任务执行器。
+    public static void main(String[] args) {
+
+    }
+}
